75d7312db6c3cb44f003325c778aa4bd59411651
madara <madara@susanoo>
2021-01-23 20:29:22 -0600

    init

---
+++ src/index.ts
@@ -0,0 +1,111 @@
+
+export interface IColor {
+    "50": string;
+    "100": string;
+    "200": string;
+    "300": string;
+    "400": string;
+    "500": string;
+    "600": string;
+    "700": string;
+    "800": string;
+    "900": string;
+    A100: string;
+    A200: string;
+    A400: string;
+    A700: string;
+}
+
+export interface IPalette {
+    red: IColor;
+    blue: IColor;
+    grey: IColor;
+    pink: IColor;
+    teal: IColor;
+    brown: IColor;
+    green: IColor;
+    amber: IColor;
+    yellow: IColor;
+    orange: IColor;
+    purple: IColor;
+    indigo: IColor;
+    bluegrey: IColor;
+    deeppurple: IColor;
+    deeporange: IColor;
+}
+
+declare global {
+    interface Window {
+        [key: string]: any;
+    }
+}
+
+export type Diff<T, U> = T extends U ? never : T;
+
+export type Require<T, K extends keyof T> = T &
+    Required<{ [P in K]: Diff<T[P], undefined> }>;
+
+export type Optional<T, K extends keyof T> = {
+    [P in K]?: Diff<T[P], undefined>;
+} &
+    Omit<T, K>;
+
+export interface RouterParams {
+    channel_id?: string;
+    workspace_id?: string;
+}
+
+export interface RouteLocation {
+    state: any;
+    search: string;
+    pathname: string;
+    hash: string;
+    key?: string;
+}
+
+export interface Route extends RouteLocation {
+    params: RouterParams;
+}
+
+export type ColumnType = "stack" | "queue";
+
+export type ThreadType = "main" | "reply" | "card";
+
+export type ChannelType = "public" | "private" | "direct";
+
+export type Id = string;
+
+export interface Unique {
+    id: Id;
+}
+
+export interface Timestamp extends String {}
+
+export interface Timestamped {
+    timestamp: Timestamp;
+}
+
+export interface Positioned {
+    position: number;
+}
+
+export interface BelongsToUser {
+    user_id: Id;
+}
+
+export interface BelongsToCard {
+    card_id: Id;
+}
+
+export interface BelongsToThread {
+    thread_id: Id;
+}
+
+export interface BelongsToChannel {
+    channel_id: Id;
+}
+
+export interface BelongsToWorkspace {
+    workspace_id: Id;
+}
+
