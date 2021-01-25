705b347ed387bae9edfeb259cbfc110922fe1b5c
mfoncho <akonumboche@gmail.com>
2021-01-25 08:34:34 -0600

    promisables

--- src/index.ts
+++ src/index.ts
@@ -50,6 +50,27 @@ export type Optional<T, K extends keyof T> = {
 } &
     Omit<T, K>;
 
+export interface Thenable<T = any> {
+    then<S, E>(
+        onFullfill: (data: T) => S,
+        onReject?: (reason: any) => E
+    ): Promise<S | E>;
+}
+
+export interface Finalizable<T = any> {
+    finally<F = never>(onFullfill: () => F): Promise<T | F>;
+}
+
+export interface Catchable<T = any> {
+    catch<F = never>(onError: () => F): Promise<T | F>;
+}
+
+export interface Promiseable<T = any, E = any>
+    extends Thenable<T>,
+    Finalizable<T>,
+    Catchable<E> {
+}
+
 export interface RouterParams {
     channel_id?: string;
     workspace_id?: string;
